24e015bc26413502fb4a37a2cb5aa65d2aee6dd5
IrinaKholkin <iryna1903@web.de>
2025-01-29 22:41:01 +0100

    Task 01

--- src/main/java/task_01/Main.java
+++ src/main/java/task_01/Main.java
@@ -1,10 +1,14 @@
 package task_01;
 
+import java.util.concurrent.Callable;
+
 public class Main {
     public static void main(String[] args) {
 
         Cat cat = new Cat("Black"); 
         System.out.println(cat);
 
+       Cat cat1 = new Cat("White");
+        System.out.println(cat1);
     }
 }
